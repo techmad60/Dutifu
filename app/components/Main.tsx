fd3a295240e5b459b225ffd016e64f15558b915b
Techmad <techmad60@gmail.com>
2024-10-23 10:21:41 -0700

    features-one done

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -8,71 +8,53 @@ export default function Main () {
          {/* Hero Section */}
          <section className="flex flex-col items-center justify-center text-center py-8 lg:text-start lg:flex-row lg:px-16 lg:py-[4rem] lg:gap-16 xl:gap-44">
           {/* Text and Button Section */}
-          <div className="flex flex-col justify-center items-center lg:justify-start lg:items-start">
-            <h1 className={`${player.className} text-color-two font-semibold px-12 text-xl my-4 lg:px-0 lg:text-4xl lg:w-[29.4375rem]`}>
-            List and grow your business with just few clicks
-            </h1>
+           <div className="flex flex-col justify-center items-center lg:justify-start lg:items-start">
+                <h1 className={`${player.className} text-color-two font-semibold px-12 text-xl my-4 lg:px-0 lg:text-4xl lg:w-[29.4375rem]`}>
+                List and grow your business with just few clicks
+                </h1>
 
-            {/* Responsive Hero Image */}
-            <Image 
-              src="/images/Hero-img.svg" 
-              alt="Hero Image"
-              className="my-4 lg:hidden" 
-              width={180} 
-              height={126.37} 
-            />
+                {/* Responsive Hero Image */}
+                <Image 
+                src="/images/Hero-img.svg" 
+                alt="Hero Image"
+                className="my-4 lg:hidden" 
+                width={180} 
+                height={126.37} 
+                />
 
-            <p className="text-sm text-color-one px-8 py-4 sm:px-12 md:px-20 lg:px-0 lg:w-[425px] lg:text-xl">
-            Eliminate manual work and save time by listing your business or service on the best business directory in Nigeria.
-            </p>
+                <p className="text-sm text-color-one px-8 py-4 sm:px-28 md:px-32 lg:px-0 lg:w-[425px] lg:text-xl">
+                Eliminate manual work and save time by listing your business or service on the best business directory in Nigeria.
+                </p>
+            </div>
 
-            
-          </div>
-
-          {/* Large Screen Hero Image */}
-          <div className="hidden lg:flex">
-            <Image 
-              src="/images/Hero-img.svg"
-              alt="Hero Image"
-              width={500}
-              height={56}
-            />
-          </div>
+            {/* Large Screen Hero Image */}
+            <div className="hidden lg:flex">
+                <Image 
+                src="/images/Hero-img.svg"
+                alt="Hero Image"
+                width={500}
+                height={56}
+                />
+            </div>
         </section>
-        
-        {/* Gemini Section */}
-        {/* <section className="flex flex-col items-center text-center my-12">
-          <h2 className={`${poppins.className} text-color-two font-medium text-xl 
-          lg:text-[2rem]`}>Your Virtual Health Assistant</h2>
-          <p className="leading-[1.875rem] text-sm text-color-one px-8 mt-4 sm:px-16 md:px-24 font-medium lg:px-0 lg:w-[36.6875rem] lg:text-xl">Get instant insights on potential health conditions based on your symptoms—powered by
-          <span className="pl-1 relative bottom-[0.24rem]">
-            <Image 
-            src={"/images/Gemini-logo 1.svg"}
-            alt="Gemini-logo"
-            className="inline"
-            width={50}
-            height={18.46}
-          /></span>.</p>
-        </section> */}
         
         {/* Features Section */}
         <section className="flex flex-col justify-center items-center space-y-12">
 
-          {/* Symptom Insights */}
-          {/* <section className="flex flex-col justify-center items-center sm:flex-row gap-8 my-8">
+          {/* Collect reviews and ratings */}
+          <section className="flex flex-col justify-center items-center sm:flex-row gap-8 my-8 lg:gap-2 xl:gap-32">
             <Image 
               src={"/images/feature-1.svg"}
               alt="Illustration"
-              className="lg:w-[307px] h-[316]"
+              className="lg:w-[592px]"
               width={180}
               height={185.28}
             />
-            <div className="flex flex-col justify-center items-center text-center">
-              <h3 className={`${poppins.className} font-medium text-color-three lg:text-2xl`}>AI-Powered Symptom Insights</h3>
-              <p className="font-light text-sm text-color-one w-[16.875625rem] my-2 lg:text-xl lg:w-[23.1875rem]">Describe your symptoms in everyday language, and let our AI provide you with potential diagnoses and next steps.</p>
-              <button className="flex items-center text-color-two gap-2 mt-8 lg:text-lg">Let&apos;s go <FaArrowRight className=""/></button>
+            <div className="flex flex-col justify-center items-center text-center lg:text-start lg:items-start">
+              <h3 className={`${player.className} font-medium text-color-two lg:text-2xl`}>Collect reviews and ratings from customers</h3>
+              <p className="font-light text-sm text-color-one w-[16.875625rem] my-2 lg:text-xl lg:w-[23.1875rem]">Accelerate your business reputation by 90% and gain social proof through reviews and ratings requested from customers.</p>
             </div>
-          </section> */}
+          </section>
 
           {/* Symptom Questionnaire */}
           {/* <section className="flex flex-col justify-center items-center sm:flex-row-reverse gap-8 my-8">
